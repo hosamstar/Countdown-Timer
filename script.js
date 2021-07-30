6b92185231694fd01a342ec93f6b039fd518c5f8
hosam shaker <hoss@x.com>
2021-07-30 13:25:23 +0200

    my changes

--- script.js
+++ script.js
@@ -3,16 +3,25 @@
 
 
 var countdown = () => {
-    let date = document.getElementsByClassName("inputDate").value;
-    let title = document.getElementsByClassName("titleText").value;
+    let date = document.querySelector(".inputDate").value;
+    let title = document.getElementById("titleText").value;
+    
+    
+
     let countDate = new Date(date).getTime(); 
     console.log(countDate);
 
-    setInterval(() => {
+    var interval = setInterval(() => {
        
+
         let now = new Date().getTime();
         let difference = countDate - now;
 
+        // if (now > countDate){
+        //     alert("This date is in the past!");
+        //     clearInterval(countdown);
+        //     return;
+        // }
         // Time Calculation
         const second = 1000;
         const minute = second * 60;
@@ -25,18 +34,42 @@ var countdown = () => {
         let textMinute = Math.floor((difference % hour)  / minute);
         let textSecond = Math.floor((difference % minute)  / second);
 
-        document.querySelector(".days").innerText = textDay;
-        document.querySelector(".hours").innerText = textHour;
-        document.querySelector(".minutes").innerText = textMinute;
-        document.querySelector(".seconds").innerText = textSecond;
+        
+
+        document.getElementById('container').innerHTML = `
+            <h2 id="eventName">${title}</h2>
+            <div class="counter_container">
+                <div class = "box_container">
+                    <div class="days boxes" >${textDay}</div>
+                    <p>Days</p>
+                </div>
+                <p>:</p>
+                <div class = "box_container">
+                    <div class="hours boxes" >${textHour}</div>
+                    <p>Hours</p>
+                </div>
+                <p>:</p>
+                <div class = "box_container">
+                    <div class="minutes boxes" >${textMinute}</div>
+                    <p>Minutes</p>
+                </div>
+                
+                <p>:</p>
+                <div class = "box_container">
+                    <div class="seconds boxes" >${textSecond}</div>
+                    <p>Seconds</p>
+                </div>
+            </div>`;
+
 
         if(difference <= 0){
-                     clearInterval(countdown);
+                     clearInterval(interval);
                      document.querySelector(".days").innerText = "00";
                      document.querySelector(".hours").innerText = "00";
                      document.querySelector(".minutes").innerText = "00";
                      document.querySelector(".seconds").innerText = "00";
                      alert("Time up!!");
+                     return;
             
                  }
     }, 1000);
@@ -44,4 +77,4 @@ var countdown = () => {
 }
 
 
-//document.getElementsByClassName("start_button").onclick = countdown;
+document.getElementById("start_button").onclick = countdown;
